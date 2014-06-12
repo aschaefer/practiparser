778d2c2c8c4fa483e0e3be6f4861e7870145d7cd
Andrew Schaefer <andrew@schaefer.nu>
2014-06-11 22:59:51 -0400

    parses properly, figuring out data structures and relationships

--- src/main/java/nu/schaefer/practiparser/json/PractiParser.java
+++ src/main/java/nu/schaefer/practiparser/json/PractiParser.java
@@ -1,17 +1,58 @@
 package nu.schaefer.practiparser.json;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class PractiParser {
 	public static void main(String[] args) throws Exception{
-		   ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
-		   //mapper.configure(org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		   
+		   ObjectMapper mapper = new ObjectMapper(); 
 		   MatchScores scores = mapper.readValue(new File("/tmp/match_scores.json"), MatchScores.class);
 		   System.out.println(scores);
 		   MatchDef definition = mapper.readValue(new File("/tmp/match_def.json"), MatchDef.class);
 		   System.out.println(definition);
+		   
+		   Map<String, MatchShooter> shooterMap = new HashMap<String,MatchShooter>();
+		   for (MatchShooter shooter: definition.getMatchShooters()){
+			   shooterMap.put(shooter.getShUuid(), shooter);
+		   }
+		   
+		   Map<String, MatchStage> stageMap = new HashMap<String,MatchStage>();
+		   for (MatchStage stage: definition.getMatchStages()){
+			   stageMap.put(stage.getStageUuid(), stage);
+		   }
+		   
+		   for (MatchScore score: scores.getMatchScores()){
+			   MatchStage stage = stageMap.get(score.getStageUuid());
+			   System.out.println("Scores for Stage " + score.getStageNumber() + " - " + stage.getStageName() );
+			  
+			   for ( StageStagescore stageScore: score.getStageStagescores()){
+				  
+				  MatchShooter shooter = shooterMap.get(stageScore.getShtr());
+				  if ( shooter != null){
+					  System.out.print(shooter.getShFn() + " " + shooter.getShLn() + " : ");
+				  } else {
+					  System.out.print("UNKNOWN SHOOTER");
+				  }
+				  
+				  if ( stageScore.getDnf()){
+					  System.out.print(" **DNF** ");
+				  }
+				  double totalTime = 0;
+				  double worstTime = 0;
+				  for(double str: stageScore.getStr()){
+					  System.out.print( str+",");
+					  totalTime += str;
+					  if ( str > worstTime){
+						  worstTime = str;
+					  }
+				  }
+				  totalTime -= worstTime;
+				  System.out.print("("+totalTime+")");
+				  System.out.println();
+			   }
+		   }
 	}
 }
